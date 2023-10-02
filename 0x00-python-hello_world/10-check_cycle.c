776abb3bcc47bad349089a31ce243c6f6364abde
amorezitha <amorezitha@gmail.com>
2023-10-02 05:22:34 -0700

    0x00. Python - Hello, World

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,10 +6,10 @@
  *
  * Return: 1 if the list has a cycle, 0 if if doesn't
  */
-inr check_cycle(listint_t *list)
+int check_cycle(listint_t *list)
 {
 	listint_t *slow = list;
-	listint_r *fast;
+	listint_t *fast;
 
 	if (!list)
 		return (0);
