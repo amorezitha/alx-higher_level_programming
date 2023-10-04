7f960346b8d5adc8c4d5152c8b7e995b53517e59
amorezitha <amorezitha@gmail.com>
2023-10-04 12:17:31 -0700

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for num in range(0, 100):
     digito1 = num / 10
     digito2 = num % 10
     if num == 89:
-        print('{:d}'.format(num))
+        print('{:02d}'.format(num))
     elif digito1 < digito2:
         print('{02d}'.format(num), end=", ")
