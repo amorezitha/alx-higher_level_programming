6867ae7bf35d6cc83a48e5744080998bca5dd342
amorezitha <amorezitha@gmail.com>
2023-10-05 06:22:41 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+
     from calculator_1 import add, sub, mul, div
-    if len(sys.argv) != 4:
+    from sys import argv, exit
+
+    # excludes name of file
+    number_of_args = len(argv) - 1
+
+    if number_of_args != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
 
-    if sys.argv[2] == "+":
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif sys.argv[2] == "-":
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif sys.argv[2] == "*":
-         print("{} * {} = {}".format(a, b, mul(a, b)))
-     elif sys.argv[2] == "/":
-         print("{} / {} = {}".format(a, b, div(a, b)))
-     else:
+    operat = argv[2]
+
+    if operat != '+' and operat != '-' and operat != '*' and operat !='/':
          print("Unknown operator. Available operators: +, -, * and /")
-         exit(1)
+           exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if operat == "+":
+        print("{:d}  {} {:d} = {}".format(a, argv2, b, add(a, b)))
+    elif operat == "-":
+        print("{:d}  {} {} = {}".format(a, ,argv[2] b, sub(a, b)))
+    elif operat == "*":
+        print("{:d}  {} {} = {}".format(a, argv[2], b, mul(a, b)))
+    else:
+        print("{:d}  {} {:d} = {}".format(a, argv[2] b, div(a, b)))
