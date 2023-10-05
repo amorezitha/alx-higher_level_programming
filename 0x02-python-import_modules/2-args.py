4358c0d26a267ec6e3eeae5202a0ee0910eec5f6
amorezitha <amorezitha@gmail.com>
2023-10-05 04:48:04 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,12 +6,12 @@ if __name__ == "__main__":
 
     # excludes name of program
     number_of_args = len(argv) - 1
-    if number_of_args ==0:
+    if number_of_args == 0:
         print(f"{number_of_args:d} arguments.")
     if number_of_args == 1:
         print(f"{number_of_args:d} argument:")
     if number_of_args > 1:
         print(f"{number_of_args:d} arguments:")
-    while count < len(argv)
+    while count < len(argv):
         print(f"{count:d}: {argv[count]}")
         count += 1
