2e3e21ca8a700efca7c0cb777a7a572b2de5b84c
amorezitha <amorezitha@gmail.com>
2023-10-05 00:59:29 -0700

    Python - import & modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pytho3
+if __name__ == "__main__":
+    from sys import argv
+    a = len(argv)
+    if len(argv) ==1:
+        print(f"{a - 1} arguments.")
+    elif len(argv) ==2:
+        print(f"{a - 1} argument:")
+    else:
+        print(f"{a - 1} arguments:")
+    for argu in range(1, a):
+        print(f"{argu}: {argv[argu]}")
