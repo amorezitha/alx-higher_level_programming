49ba129bc65ee709d5425db909dc17d84db5915f
amorezitha <amorezitha@gmail.com>
2023-10-05 04:16:29 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     from sys import argv
     a = len(argv)
     if len(argv) ==1:
-        print(f"{a - 1} arguments.")
+        print(f"{a - 1} arguments:")
     elif len(argv) ==2:
-        print(f"{a - 1} argument:")
+        print(f"{a - 1} arguments:")
     else:
         print(f"{a - 1} arguments:")
     for argu in range(1, a):
