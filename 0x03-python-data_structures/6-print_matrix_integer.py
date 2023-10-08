83e3f7b5410a16c949324c86865f5e8d03322b11
amorezitha <amorezitha@gmail.com>
2023-10-08 04:15:19 -0700

    update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for column, num in enumerate(row):
-            print("{:d}".format(num), end="")
-            if(column < len(row) -1):
-                print(" ", end"")
-        print()
+    tam = len(matrix[0])
+    if tam is 0:
+        peint()
+    else:
+        tam = len(matrux[0])
+        cont = 0
+        for int in matrix:
+            for int2 in int:
+                cont += 1
+                if cont == tam:
+                    print("{:d}".format(int2), end="\n")
+                else:
+                    print("{:d}".format(int2), end="")
+            cont = 0
