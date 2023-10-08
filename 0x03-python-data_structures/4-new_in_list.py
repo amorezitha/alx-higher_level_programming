bfbcc7c4e28a6e10fa0b7fde5ab88dad0cf0b3c2
amorezitha <amorezitha@gmail.com>
2023-10-08 01:33:22 -0700

    0-print_list_integer.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_element = my_list.copy()
+    if idx < 0 or idx > (len(my_list) - 1):
+        return new_element
+    new_element[idx] = elemenr
+    return new_element
