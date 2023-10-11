0353658277ff56dcc93486d3d7f594271c42b66d
amorezitha <amorezitha@gmail.com>
2023-10-11 07:48:46 -0700

    update

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dir = dict(a_dictionary)
-    for i in a_dictionary:
-        new_dir[i] = 2
+    for i, k in a_dictionary:
+        new_dir[i] =k * 2
         return new_dir
