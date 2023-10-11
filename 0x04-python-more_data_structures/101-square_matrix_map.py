1c85bade58278b3c2261289b3f812d97ebcbeb76
amorezitha <amorezitha@gmail.com>
2023-10-11 12:03:19 -0700

    update

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return (ist(lambda value: list(map(lambda v2: v2**2, value)), matrix))
+    return (ist(map(lambda value: list(map(lambda v2: v2**2, value)), matrix))
