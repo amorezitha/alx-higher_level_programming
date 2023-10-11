ef9567a6a2de47b021aa2212eb55a0de8ca023f1
amorezitha <amorezitha@gmail.com>
2023-10-11 12:37:43 -0700

    update

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = {n: a_dictionary[n] * 2 for n in a_dictionary}
-    return new_dictt
+    multiple = {}
+    for i in a_dictionary:
+        multiple[i] = a_dictionary[i] * 2
+    return multiple
