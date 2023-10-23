4e1c712ce7a591dcc24de71b35eba84a97999e31
amorezitha <amorezitha@gmail.com>
2023-10-23 08:37:17 -0700

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 safe_print_list(my_list=[], x=0):
-    j = 0
-    for i in range(0, x):
-        try:
-            print(f'{my_list[i]', end="")
-            j += 1
-        except IndexError:
-            break
-        print("")
-        return (j)
+    """Prints x elements of a list.
+
+    Args:
+        my_list (list): The list to ptint elements from
+        x (int): The number of elements of my_list to print
+
+        Returns:
+            The number of elements printed
+        """
+        j = 0
+        for i in range(0, x):
+            try:
+                print(f'{my_list[i]', end="")
+                j += 1
+            except IndexError:
+                break
+            print("")
+            return (j)
