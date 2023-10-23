bf5e95098b0dd1064da88220bfcf437d999b724e
amorezitha <amorezitha@gmail.com>
2023-10-23 10:50:57 -0700

    update

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
@@ -16,4 +18,4 @@ def list_division(my_list_1, my_list_2, list_length):
         finally:
             new_list.append(result)
 
-            return (new_list)
+        return (new_list)
