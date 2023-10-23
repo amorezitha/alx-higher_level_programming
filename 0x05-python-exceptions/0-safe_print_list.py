76b46b84fa93cb7e5bc473900ffa3365e750ed39
amorezitha <amorezitha@gmail.com>
2023-10-23 08:26:10 -0700

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ safe_print_list(my_list=[], x=0):
         try:
             print(f'{my_list[i]', end="")
             j += 1
-        except (IndexError):
+        except IndexError:
             break
         print("")
         return (j)
